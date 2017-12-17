0ab0bdcb81b28251a6a484b31f219a77cde9351e
Hugo Vale Pereira <hvpereira@gmail.com>
2017-12-17 04:02:43 +0000

    abc cresce

--- DNA.js
+++ DNA.js
@@ -1,13 +1,25 @@
 
 class DNA{
-  constructor(){
-    this.length=1000;
-    this.genes = [];
+  constructor( letra ){
+    if(letra){
+      if(letra=='a'){
+        this.genes =[0,0,0,0,0,-1,1,0,0,0,0,0,0,1,0,0,-1,1,0,-1];
 
-    for (var i = 0; i<this.length; i++) {
-      this.genes[i]=myp5.round(myp5.random(-1, 1));
+      }else if(letra=='b'){
+        this.genes =[-1,0,1,1,0,0,0,0,-1,-1,0,1,0,0,1,0,-1,-1,-1,0];
+
+      }else if(letra=='c'){
+        this.genes =[1,1,0,0,0,0,-1,-1,-1,1,0,-1,0,-1,0,-1,0,-1,0,0];
+      }
+    }else{
+      this.length=200;
+      this.genes = [];
+
+      for (var i = 0; i<this.length; i++) {
+        this.genes[i]=myp5.round(myp5.random(-1, 1));
+      }
     }
-    
+
   }
 
 
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 var s = function(sketch){
 
   var d;
-  var p;
+  var a, b,c;
 
   var f;
 
@@ -14,8 +14,11 @@ var s = function(sketch){
     sketch.createCanvas(window.innerWidth,window.innerHeight);
 
     sketch.f =sketch.color(255, 200, 0);
-    sketch.d= new DNA();
-    sketch.p= new Planta(sketch.round((sketch.width/2)/15), sketch.round((sketch.height/2)/15), 0, -1, sketch.d, sketch.f);
+    sketch.d= new DNA('a');
+    sketch.a= new Planta(sketch.round((sketch.width/2)/25), sketch.round((sketch.height/2)/25), 0, -1, sketch.d, sketch.f);
+    sketch.b= new Planta(sketch.round((sketch.width/2)/25+5), sketch.round((sketch.height/2)/25-3), 1, 0, new DNA('b'), sketch.color(200, 0, 255));
+    sketch.c= new Planta(sketch.round((sketch.width/2)/25+10), sketch.round((sketch.height/2)/25-5), 0, -1, new DNA('c'), sketch.color(200, 0, 255));
+
 
   };
 
@@ -23,12 +26,17 @@ var s = function(sketch){
   sketch.draw= function(){
     sketch.clear();
     //sketch.background(255,255,255,0);
-    sketch.scale(15, 15);
+    sketch.scale(25, 25);
 
-    sketch.p.plant();
-    sketch.p.grow();
+    sketch.a.plant();
+    sketch.a.grow();
+    sketch.b.plant();
+    sketch.b.grow();
+    sketch.c.plant();
+    sketch.c.grow();
 
-    if (sketch.p.cau.length==sketch.comp) {
+
+    if (sketch.a.cau.length==14) {
 
       sketch.noLoop();
     }
